d074c86ae8bded0c12773854c8b1aa61c5e25bdc
yjw <514305919@qq.com>
2019-07-24 09:54:53 +0800

    no message

---
+++ 111/111/111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  111
+//
+//  Created by YY on 2019/7/24.
+//  Copyright © 2019 ansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 111/111/111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  111
+//
+//  Created by YY on 2019/7/24.
+//  Copyright © 2019 ansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
